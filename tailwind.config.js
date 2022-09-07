15f433cc0e3edeb5f91298ab947bd1ee0b108d05
cedricquitor <cedricquitor.dev@gmail.com>
2022-09-08 06:15:50 +0800

    Added tailwindcss as dependency

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "safe-white": "#FDFDFD",
+      "safe-black": "#2F2F2F",
+      "safe-gray": "#EFEFEF",
+      "primary-green": "#52B286",
+      "secondary-green": "#40916C",
+      "primary-navy": "#3F3D56",
+      "primary-gray": "#AEB4B6",
+      "secondary-gray": "#DADADA",
+      "bg-color": "#F9F7F4",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
