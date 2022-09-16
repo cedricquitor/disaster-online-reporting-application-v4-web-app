2c4cb5fa4e50202b5afc2c9757a4d2be5bdd7ecb
cedricquitor <cedricquitor.dev@gmail.com>
2022-09-17 04:02:52 +0800

    Added google map API for map.jsx

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -6,12 +6,21 @@ import { IoLogOut } from "react-icons/io5";
 import { useAuthContext } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import googleMapReact from "google-map-react";
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 import { toast } from "react-toastify";
+import Loading from "../components/Loading";
 
 const Map = () => {
   const { testContext, logout } = useAuthContext();
 
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyAaerEd8Q4K2BQgRVHh4kVXE9YKolhQ5xI",
+  });
+
   const navigate = useNavigate();
+
+  // Center of map
+  const defaultCoordinates = { lat: 14.609817753661206, lng: 120.98943292296173 };
 
   const handleLogout = async () => {
     try {
@@ -23,12 +32,16 @@ const Map = () => {
     }
   };
 
+  if (!isLoaded) {
+    return <Loading />;
+  }
+
   return (
     <div className="bg-bg-color flex items-start">
       {/* Navigation */}
-      <aside className="flex flex-col justify-between h-screen">
+      <aside className="flex flex-col justify-between h-screen fixed z-10">
         {/* Upper Part */}
-        <div className="h-[80%] w-24 bg-safe-gray rounded-br-2xl">
+        <div className="h-[80%] w-24 bg-safe-gray rounded-br-2xl outline outline-2 outline-primary-gray">
           <div className="overflow-y-auto py-4 px-4">
             <ul className="space-y-2">
               <li>
@@ -56,7 +69,7 @@ const Map = () => {
           </div>
         </div>
         {/* Bottom Part */}
-        <div className="h-[15%] w-24 bg-safe-gray rounded-tr-2xl">
+        <div className="h-[15%] w-24 bg-safe-gray rounded-tr-2xl outline outline-2 outline-primary-gray">
           <a href="#" onClick={() => handleLogout()} className="text-secondary-gray transition hover:text-primary-green active:text-secondary-green">
             {/* <img src="src/assets/logout_btn.svg" alt="Logout Button" className="mx-auto mt-8" /> */}
             <IoLogOut className="h-12 w-12 mt-8 mx-auto" />
@@ -64,7 +77,22 @@ const Map = () => {
         </div>
       </aside>
       {/* Main Content Area */}
-      <h1>Map</h1>
+      <div className="h-screen w-full">
+        {/* <googleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAaerEd8Q4K2BQgRVHh4kVXE9YKolhQ5xI" }}
+        defaultCenter={defaultCoordinates}
+        center={defaultCoordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></googleMapReact> */}
+        <GoogleMap center={defaultCoordinates} zoom={16} mapContainerStyle={{ width: "100%", height: "100%" }} options={{ zoomControl: false, streetViewControl: false, mapTypeControl: false, fullscreenControl: false }}>
+          {/* Markers */}
+          <Marker position={defaultCoordinates} icon={DoraHomeIcon} />
+        </GoogleMap>
+      </div>
     </div>
   );
 };
