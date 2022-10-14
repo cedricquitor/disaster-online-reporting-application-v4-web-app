967f450c66d2308be13cc5bdad534333ad9618bd
cedricquitor <cedricquitor.dev@gmail.com>
2022-10-14 22:33:28 +0800

    Map now displays evacuation centers

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DoraHomeIcon from "../assets/dora_home_btn.svg";
 import { HiSearch, HiFolder, HiOfficeBuilding } from "react-icons/hi";
 import { FaMapMarked } from "react-icons/fa";
 import { IoLogOut } from "react-icons/io5";
 import { useAuthContext } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete } from "@react-google-maps/api";
+import { useJsApiLoader, GoogleMap, MarkerF, Autocomplete, Marker } from "@react-google-maps/api";
 import { toast } from "react-toastify";
 import Loading from "../components/Loading";
+import { onValue, ref } from "firebase/database";
+import { db } from "../configs/firebase";
 
 const Map = () => {
+  // Reports and evacuation data state manager
+  const [ecData, setEcData] = useState([]);
+  const [tempEcData, setTempEcData] = useState([]);
+  const [reportData, setReportData] = useState([]);
+
+  // Loading state manager
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Instantiate AuthContext for use
   const { testContext, logout } = useAuthContext();
 
   // List of libraries to inject to JavaScript API Loader
   const [libraries] = useState(["places"]);
-
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    libraries,
-  });
 
   const navigate = useNavigate();
 
@@ -35,7 +41,42 @@ const Map = () => {
     }
   };
 
-  if (!isLoaded) {
+  // Query evacuation centers
+  const getEvacuationCenters = async () => {
+    setIsLoading(true);
+    const dbRef = ref(db, "/EvacuationCenters");
+
+    const queryReports = await onValue(dbRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const ec = snapshot.val();
+        const ecList = [];
+        for (let id in ec) {
+          ecList.push({ id, ...ec[id] });
+        }
+        setEcData(ecList);
+        setTempEcData(ecList);
+        console.log(ecList);
+
+        // After getting data, set loading to false
+        setIsLoading(false);
+      } else {
+        console.error("No data");
+      }
+    });
+  };
+
+  // Fetch evacuation centers at page load
+  useEffect(() => {
+    getEvacuationCenters();
+  }, []);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    libraries,
+  });
+
+  // If any of the loading state is True, return Loading.jsx component
+  if (!isLoaded || isLoading) {
     return <Loading />;
   }
 
@@ -82,10 +123,18 @@ const Map = () => {
         </div>
       </aside>
       {/* Main Content Area */}
-      <div className="h-screen w-full">
+      <div className="absolute flex w-screen top-[5%] justify-center">
+        <div className="bg-bg-color p-8 z-10">
+          <h1>Sort Evacuation Center by City</h1>
+        </div>
+      </div>
+      <div className="h-screen w-full z-0">
         <GoogleMap center={defaultCoordinates} zoom={16} mapContainerStyle={{ width: "100%", height: "100%" }} options={{ zoomControl: false, streetViewControl: false, mapTypeControl: false, fullscreenControl: false }}>
           {/* Markers */}
-          <Marker position={defaultCoordinates} icon={DoraHomeIcon} />
+          {ecData?.map((evacuationCenter) => {
+            const location = { lat: evacuationCenter.latitude, lng: evacuationCenter.longitude };
+            return <MarkerF position={location} icon={DoraHomeIcon} />;
+          })}
         </GoogleMap>
       </div>
     </div>
