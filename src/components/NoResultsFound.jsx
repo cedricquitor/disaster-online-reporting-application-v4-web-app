75ecc2393dc3d8345be8f0b0ad06fb0bf3b499ab
cedricquitor <cedricquitor.dev@gmail.com>
2022-10-18 04:22:37 +0800

    Removed Pagination.jsx and added NoResultsFound.jsx

---
+++ src/components/NoResultsFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoResultsFound = () => {
+  return (
+    <div>
+      <h1>NoResultsFound</h1>
+    </div>
+  );
+};
+
+export default NoResultsFound;
--- src/helpers/Pagination.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-const Pagination = () => {
-  return <div>Pagination</div>;
-};
-
-export default Pagination;
