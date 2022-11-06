6642f2c838e03c209321401acf0b12ab801254eb
cedricquitor <cedricquitor.dev@gmail.com>
2022-11-06 16:42:02 +0800

    Refactored UI of the navigation in the menu

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -124,44 +124,34 @@ const Menu = () => {
           <p className="text-2xl font-bold text-primary-green">What's the task today?</p>
           <p className="text-sm text-primary-gray">Administrators are reponsible of managing the evacuation centers and ensuring the integrity of the reports by making sure that all reports follow the guidelines</p>
         </div>
-        {/* First 2 Top */}
-        <div className="flex flex-row gap-4">
-          {/* Left */}
-          <Link to="/evacuation" className="w-[12rem] h-[12rem] rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
-            <div className="flex justify-between">
-              <p className="w-[50%] text-secondary-green text-xl font-bold">Manage Evacuation Center</p>
-              <HiOfficeBuilding className="h-8 w-8 text-secondary-green" />
+        {/* Menu Items */}
+        <Link to="/evacuation" className="w-full rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <p className="text-secondary-green text-xl font-bold">Manage Evacuation Center</p>
+              <p className="text-base text-primary-gray">Add or update Evacuation Center details</p>
             </div>
-            <p className="mt-2 text-base text-primary-gray">Add or update Evacuation Center details</p>
-          </Link>
-          {/* Right */}
-          <Link to="/reports" className="w-[12rem] h-[12rem] rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
-            <div className="flex justify-between">
-              <p className="w-[50%] text-secondary-green text-xl font-bold">Manage Reports</p>
-              <HiFolder className="h-8 w-8 text-secondary-green" />
-            </div>
-            <p className="mt-2 text-base text-primary-gray">Validate the integrity of the reports</p>
-          </Link>
-        </div>
-        {/* Last 2 Bottom */}
-        <div className="flex flex-row gap-4">
-          {/* Left */}
-          <Link to="map" className="w-[12rem] h-[12rem] rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
-            <div className="flex justify-between">
-              <p className="w-[50%] text-secondary-green text-xl font-bold">View Map</p>
-              <FaMapMarked className="h-8 w-8 text-secondary-green" />
-            </div>
-            <p className="mt-2 text-base text-primary-gray">Check the status of the map</p>
-          </Link>
-          {/* Right */}
-          <div className="w-[12rem] h-[12rem] rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
-            <div className="flex justify-between">
-              <p className="w-[50%] text-secondary-green text-xl font-bold">View Users</p>
-              <HiFolder className="h-8 w-8 text-secondary-green" />
-            </div>
-            <p className="mt-2 text-base text-primary-gray">Manage the app's users</p>
+            <HiOfficeBuilding className="h-8 w-8 my-auto text-secondary-green" />
           </div>
-        </div>
+        </Link>
+        <Link to="/reports" className="w-full rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <p className="text-secondary-green text-xl font-bold">Manage Reports</p>
+              <p className="text-base text-primary-gray">Validate the integrity of the reports</p>
+            </div>
+            <HiFolder className="h-8 w-8 my-auto text-secondary-green" />
+          </div>
+        </Link>
+        <Link to="/map" className="w-full rounded-xl p-2 bg-safe-gray cursor-pointer shadow-md transition border border-secondary-gray hover:shadow-xl hover:bg-secondary-gray">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <p className="text-secondary-green text-xl font-bold">View Map</p>
+              <p className="text-base text-primary-gray">Check the status of the map</p>
+            </div>
+            <FaMapMarked className="h-8 w-8 my-auto text-secondary-green" />
+          </div>
+        </Link>
         <button onClick={handleSendPushNotifModal} className="w-full bg-primary-green mt-2 py-3 rounded-full font-bold text-xl text-safe-white shadow-lg transition hover:bg-secondary-green">
           Send push notif to users
         </button>
