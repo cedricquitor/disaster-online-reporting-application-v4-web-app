aa6f27ccb4577acbd8d48f46bb143823fa54301f
cedricquitor <cedricquitor.dev@gmail.com>
2022-12-02 14:24:46 +0800

    Initialized GenerateReport.jsx

---
+++ src/components/GenerateReport.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GenerateReport = ({ data, type }) => {
+  return (
+    <div>
+      <h1>Generate Report</h1>
+    </div>
+  );
+};
+
+export default GenerateReport;
