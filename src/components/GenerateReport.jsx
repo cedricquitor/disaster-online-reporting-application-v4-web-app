cab0118d07999ba86503f5bccb1c7241ec4588c4
cedricquitor <cedricquitor.dev@gmail.com>
2022-12-02 18:12:45 +0800

    Added react-to-print as dev dependency

--- src/components/GenerateReport.jsx
+++ src/components/GenerateReport.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import DoraLogo from "../assets/dora_logo.svg";
 
 const GenerateReport = ({ data, type }) => {
+  const componentRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: type ? type : "default",
+  });
+
   return (
-    <div>
-      <h1>Generate Report</h1>
+    <div ref={componentRef} className="bg-safe-white h-screen flex justify-center">
+      <button onClick={() => handlePrint()}>Test Button</button>
+      <div className="flex flex-col">
+        <div>
+          <img src={DoraLogo} alt="DORAv4 Home Button" className="w-[8rem] h-[8rem]" />
+        </div>
+        <div>Table</div>
+      </div>
     </div>
   );
 };
